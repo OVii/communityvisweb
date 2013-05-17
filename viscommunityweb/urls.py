b4519b710b163112a9f42e98cfd62ae98ca4fa63
Eamonn Maguire <eamonnmag@gmail.com>
2013-05-17 18:02:23 +0100

    Changed account management to use django-profiles and django-registration. Started on the profile page for a user. Will clean up rest of pages asap.

--- viscommunityweb/urls.py
+++ viscommunityweb/urls.py
@@ -2,32 +2,33 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'viscommunityweb.views.home', name='home'),
-    # url(r'^viscommunityweb/', include('viscommunityweb.foo.urls')),
+                       # Examples:
+                       # url(r'^$', 'viscommunityweb.views.home', name='home'),
+                       # url(r'^viscommunityweb/', include('viscommunityweb.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-	url(r'^web/$', 'web.views.index'),
-	url(r'^web/taxonomy/$', 'web.views.taxonomy'),
-	url(r'^web/taxonomy_alpha/$', 'web.views.taxonomy_alpha'),
-	url(r'^web/taxonomy/(?P<taxonomy_id>\d+)/$', 'web.views.taxonomy_detail'),
-	url(r'^web/request_ownership/(?P<taxonomy_id>\d+)/$', 'web.views.request_ownership'),
-	url(r'^web/request_ownership/send/$', 'web.views.request_ownership_send'),
-	url(r'^web/contact/$', 'web.views.contact'),
-	url(r'^web/references/$', 'web.views.references'),
-    url(r'^web/register/$', 'web.views.register'),
-    url(r'^web/sign_in/$', 'web.views.sign_in'),
-    url(r'^web/do_login/$', 'web.views.login'),
-	url(r'^web/do_logout/$', 'web.views.logout'),
-    url(r'^web/search/$', 'web.views.search'),
-	url(r'^web/account/$','web.views.account'),
-	#url(r'^web/references/(?P<reference_id>\d+)/$', 'web.views.reference_detail')
+                       url(r'^accounts/', include('registration.backends.default.urls')),
+                       url(r'^profiles/', include('profiles.urls')),
+                       url(r'^$', 'web.views.index'),
+                       url(r'^accounts/profile', 'web.views.profile'),
+                       url(r'^taxonomy/$', 'web.views.taxonomy'),
+                       url(r'^taxonomy_alpha/$', 'web.views.taxonomy_alpha'),
+                       url(r'^taxonomy/(?P<taxonomy_id>\d+)/$', 'web.views.taxonomy_detail'),
+                       url(r'^request_ownership/(?P<taxonomy_id>\d+)/$', 'web.views.request_ownership'),
+                       url(r'^request_ownership/send/$', 'web.views.request_ownership_send'),
+                       url(r'^contact/$', 'web.views.contact'),
+                       url(r'^references/$', 'web.views.references'),
+                       url(r'^search/$', 'web.views.search'),
+                       url(r'^account/$', 'web.views.account'),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+                       #url(r'^web/references/(?P<reference_id>\d+)/$', 'web.views.reference_detail')
+
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
 )
--- web/views.py
+++ web/views.py
@@ -22,55 +22,48 @@ email_to = "simon.walton@oerc.ox.ac.uk"
 
 # index page
 def index(request):
-	recent_items = TaxonomyItem.objects.exclude(detail_html="").order_by('last_updated')[:3]
-	return render_to_response("templates/index.html", { 'recent_taxonomy_items': recent_items }, context_instance=RequestContext(request))
-
-# sign in page.
-def sign_in(request):
-    t = loader.get_template("templates/signin.html")
-    return HttpResponse(t.render(RequestContext(request)))
-
-# register page.
-def register(request):
-    t = loader.get_template("templates/register.html")
-    return HttpResponse(t.render(RequestContext(request)))
+    recent_items = TaxonomyItem.objects.exclude(detail_html="").order_by('last_updated')[:3]
+    return render_to_response("templates/index.html", {'recent_taxonomy_items': recent_items},
+                              context_instance=RequestContext(request))
 
 # taxonomy list
 def taxonomy(request):
     taxonomies = TaxonomyCategory.objects.all()
     return render_to_response("templates/taxonomy.html", {'taxonomies': taxonomies},
-        context_instance=RequestContext(request))
+                              context_instance=RequestContext(request))
+
 
 def taxonomy_alpha(request):
     index = taxonomy_backend.alphabetic_index()
     return render_to_response("templates/taxonomy-alpha.html", {'index': index},
-        context_instance=RequestContext(request))
+                              context_instance=RequestContext(request))
 
 # taxonomy detail page
 def taxonomy_detail(request, taxonomy_id):
-	taxonomy = get_object_or_404(TaxonomyItem, pk=taxonomy_id)
-	html = html_format(taxonomy.detail_html)
-   
-	if len(html) == 0:
-		html = "There is currently no information on this taxonomy item."
-	
-	refer = reference_backend.sorted_reference_list(request, reference_entries(taxonomy.detail_html))
-	bibtex_texts = json.dumps([x.bibtex for x in refer])
+    taxonomy = get_object_or_404(TaxonomyItem, pk=taxonomy_id)
+    html = html_format(taxonomy.detail_html)
 
-	print "Got %i refs" % len(refer)
+    if len(html) == 0:
+        html = "There is currently no information on this taxonomy item."
 
-	return render_to_response("templates/taxonomy_detail.html",
-		{'taxonomy': taxonomy, 'formatted_detail': html, 'references': refer, 'bibtex_texts': bibtex_texts}, context_instance=RequestContext(request))
+    refer = reference_backend.sorted_reference_list(request, reference_entries(taxonomy.detail_html))
+    bibtex_texts = json.dumps([x.bibtex for x in refer])
+
+    print "Got %i refs" % len(refer)
+
+    return render_to_response("templates/taxonomy_detail.html",
+                              {'taxonomy': taxonomy, 'formatted_detail': html, 'references': refer,
+                               'bibtex_texts': bibtex_texts}, context_instance=RequestContext(request))
 
 # contact page for ownership request
 def request_ownership(request, taxonomy_id):
     taxonomy = get_object_or_404(TaxonomyItem, pk=taxonomy_id)
     return render_to_response("templates/contact.html", {'taxonomy': taxonomy},
-        context_instance=RequestContext(request))
+                              context_instance=RequestContext(request))
 
 # general contact page
 def contact(request):
-	return render_to_response("templates/contact.html", context_instance=RequestContext(request))
+    return render_to_response("templates/contact.html", context_instance=RequestContext(request))
 
 # accessed through a POST to send email to the admins regarding above
 def request_ownership_send(request):
@@ -81,34 +74,24 @@ def request_ownership_send(request):
 
     try:
         send_mail(email_subject, email_body, email_from,
-            [item[1] for item in settings.ADMINS], fail_silently=False)
+                  [item[1] for item in settings.ADMINS], fail_silently=False)
     except:
         return render_to_response("templates/contact.html",
-                {'error_message': "There was a problem sending the email. Please ensure all fields are filled in correctly. Please contact " +
-                                  settings.ADMINS[0][1] + " if the problem continues."},
-            context_instance=RequestContext(request))
+                                  {
+                                  'error_message': "There was a problem sending the email. Please ensure all fields are filled in correctly. Please contact " +
+                                                   settings.ADMINS[0][1] + " if the problem continues."},
+                                  context_instance=RequestContext(request))
     else:
         return render_to_response("templates/contact.html", {'success_message': 'Your request was sent successfully!'},
-            context_instance=RequestContext(request))
+                                  context_instance=RequestContext(request))
 
 # big list of all references in database (future: sorting/filtering etc)
 def references(request):
-	refs = reference_backend.sorted_reference_list(request)
+    refs = reference_backend.sorted_reference_list(request)
 
-	return render_to_response("templates/references.html", {'references': refs, 'bibtex_texts': json.dumps([x.bibtex for x in refs])},
-        context_instance=RequestContext(request))
-
-# login POST
-def login(request):
-    user = authenticate(username=request.POST['user[email]'], password=request.POST['user[password]'])
-    if user is not None:
-        django_login(request, user)
-    return render_to_response("templates/index.html", context_instance=RequestContext(request))
-
-# logout POST
-def logout(request):
-    django_logout(request)
-    return render_to_response("templates/index.html", context_instance=RequestContext(request))
+    return render_to_response("templates/references.html",
+                              {'references': refs, 'bibtex_texts': json.dumps([x.bibtex for x in refs])},
+                              context_instance=RequestContext(request))
 
 # search
 def search(request):
@@ -121,10 +104,18 @@ def search(request):
         if searchTerm in reference.bibtex:
             searchResult.append(reference)
 
-    return render_to_response("templates/search.html", {"searchTerm":searchTerm, "size":len(searchResult), "searchResults": searchResult},
-        context_instance=RequestContext(request))
+    return render_to_response("templates/search.html",
+                              {"searchTerm": searchTerm, "size": len(searchResult), "searchResults": searchResult},
+                              context_instance=RequestContext(request))
 
-# account
+
 def account(request):
-	items_owned = TaxonomyItem.objects.filter(owners__id__contains = request.user.id) if request.user.id is not None else None
-	return render_to_response("templates/account.html", {'items_owned': items_owned }, context_instance=RequestContext(request))	
+    items_owned = TaxonomyItem.objects.filter(
+        owners__id__contains=request.user.id) if request.user.id is not None else None
+    return render_to_response("templates/account.html", {'items_owned': items_owned},
+                              context_instance=RequestContext(request))
+
+
+def profile(request):
+    return render_to_response("templates/profile.html", {},
+                              context_instance=RequestContext(request))
