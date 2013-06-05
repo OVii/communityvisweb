648e769d7845a06faac8f4d782cec9f83d7d7adf
Eamonn Maguire <eamonnmag@gmail.com>
2013-06-05 09:47:38 -0700

    Showing which items have data and which haven't.

--- web/taxonomy_backend.py
+++ web/taxonomy_backend.py
@@ -4,12 +4,11 @@ from web.models import TaxonomyArea, TaxonomyCategory, TaxonomyItem, Reference
  Builds a dictionary (letter[char]) => { item, item } representing the taxonomy in the system
 """
 
-
 def alphabetic_index():
     idx = dict()
     # categories
     for item in list(TaxonomyItem.objects.all()):
-        val = (item.id, item.name, item.category.name, item.detail)
+        val = (item.id, item.name, item.category.name, item.detail, len(item.references.all()))
         if idx.has_key(item.name[0]):
             idx[item.name[0]].append(val)
         else:
