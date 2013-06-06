8594c6019a3fea8958afac0b7f753cdf0b8306ad
Eamonn Maguire <eamonnmag@gmail.com>
2013-06-05 17:24:56 -0700

    Attempting to solve the problem with the password reset page.

--- viscommunityweb/urls.py
+++ viscommunityweb/urls.py
@@ -13,13 +13,13 @@ urlpatterns = patterns('',
                        # Uncomment the admin/doc line below to enable admin documentation:
                        # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+                       url(r'^admin/', include(admin.site.urls), name='admin'),
                        url(r'^accounts/', include('registration.backends.default.urls')),
                        url(r'^profiles/', include('profiles.urls'), name='profiles'),
 
                        url(r'^$', 'web.views.index', name='community-home'),
                        url(r'^accounts/profile', 'web.views.profile', name='profile'),
                        url(r'^profile/(\w+)', 'web.views.public_profile', name='pub_profile'),
-
 
 
                        url(r'^taxonomy/add/$', 'web.views.taxonomy_add', name='add_taxonomy'),
@@ -31,21 +31,28 @@ urlpatterns = patterns('',
                        url(r'^reference/add/file$', 'web.views.reference_add_upload_file', name='upload_bibtex'),
                        url(r'^reference/add/text$', 'web.views.reference_add_upload_text', name='upload_bibtex_text'),
 
-                       url(r'^reference/remove/(?P<taxonomy_id>\d+)/(?P<reference_id>\d+)/$', 'web.views.reference_remove', name='remove_reference'),
-                       url(r'^reference/(?P<reference_id>\d+)/$', 'web.views.reference_detail', name='reference_detail'),
+                       url(r'^reference/remove/(?P<taxonomy_id>\d+)/(?P<reference_id>\d+)/$',
+                           'web.views.reference_remove', name='remove_reference'),
+                       url(r'^reference/(?P<reference_id>\d+)/$', 'web.views.reference_detail',
+                           name='reference_detail'),
 
                        url(r'^taxonomy/$', 'web.views.taxonomy', name='taxonomy'),
                        url(r'^taxonomy_alpha/$', 'web.views.taxonomy_alpha', name='taxonomy_alpha'),
                        url(r'^taxonomy/(?P<taxonomy_id>\d+)/$', 'web.views.taxonomy_detail', name='taxonomy_detail'),
                        url(r'^taxonomy/download/(\d+)/$', 'web.views.taxonomy_download', name='taxonomy_download'),
 
-                       url(r'^request_ownership/(?P<taxonomy_id>\d+)$', 'web.views.request_ownership_send', name='request_ownership'),
-                       url(r'^request_ownership/response/(?P<approval_id>\d+)$', 'web.views.request_ownership_response', name='request_ownership_response'),
-                       url(r'^revoke_ownership/(?P<taxonomy_id>\d+)$', 'web.views.revoke_ownership', name='revoke_ownership'),
+                       url(r'^request_ownership/(?P<taxonomy_id>\d+)$', 'web.views.request_ownership_send',
+                           name='request_ownership'),
+                       url(r'^request_ownership/response/(?P<approval_id>\d+)$', 'web.views.request_ownership_response',
+                           name='request_ownership_response'),
+                       url(r'^revoke_ownership/(?P<taxonomy_id>\d+)$', 'web.views.revoke_ownership',
+                           name='revoke_ownership'),
 
                        url(r'^enquiry/(?P<taxonomy_id>\d+)/$', 'web.views.handleTaxonomyEnquiry', name='enquiry'),
-                       url(r'^enquiry/(?P<taxonomy_id>\d+)/(?P<reference_id>\d+)/$', 'web.views.handleReferenceEnquiry', name='enquiry_reference'),
-                       url(r'^enquiry/(?P<decision>\w+)/(?P<enquiry_id>\d+)/$', 'web.views.respondToTaxonomyEnquiry', name='enquiry_decision'),
+                       url(r'^enquiry/(?P<taxonomy_id>\d+)/(?P<reference_id>\d+)/$', 'web.views.handleReferenceEnquiry',
+                           name='enquiry_reference'),
+                       url(r'^enquiry/(?P<decision>\w+)/(?P<enquiry_id>\d+)/$', 'web.views.respondToTaxonomyEnquiry',
+                           name='enquiry_decision'),
 
                        url(r'^contact/$', 'web.views.contact', name='contact'),
                        url(r'^contact/send', 'web.views.contact_send', name='contact_send'),
@@ -59,5 +66,5 @@ urlpatterns = patterns('',
                        #url(r'^web/references/(?P<reference_id>\d+)/$', 'web.views.reference_detail')
 
                        # Uncomment the next line to enable the admin:
-                       url(r'^admin/', include(admin.site.urls), name='admin'),
+
 )
