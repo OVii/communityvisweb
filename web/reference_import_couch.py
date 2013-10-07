7b36ef71d8084132f01f86dec8908838dfaa38bf
Simon Walton <simon.walton@oerc.ox.ac.uk>
2013-10-07 12:05:44 +0100

    Improved the bibtex parsing to include all found fields in the final reference object that CouchDB gets.

--- web/reference_import_couch.py
+++ web/reference_import_couch.py
@@ -9,74 +9,73 @@ logger = logging.getLogger(__name__)
 
 
 def getTitle(fields):
-    titleAliases = ['title', 'Title', 'TITLE']
-    print fields
-    for alias in titleAliases:
-        if fields[alias]:
-            return bibtex_purify(fields[alias])
+	titleAliases = ['title', 'Title', 'TITLE']
+	print fields
+	for alias in titleAliases:
+		if fields[alias]:
+			return bibtex_purify(fields[alias])
 
 
 def bibtex_import(filename, taxonomyItem):
-    family = ReferenceFamily(taxonomyItem.int_pk())
-    parser = bib_in.Parser()
-    bib_data = parser.parse_file(filename)
-    writer = bib_out.Writer(encoding='ascii')
+	family = ReferenceFamily(taxonomyItem.int_pk())
+	parser = bib_in.Parser()
+	bib_data = parser.parse_file(filename)
+	writer = bib_out.Writer(encoding='ascii')
 
-    for key in bib_data.entries.keys():
-        stream = StringIO.StringIO()
-        writer.write_entry(key, bib_data.entries[key], stream)
-        title = getTitle(bib_data.entries[key].fields)
-        ref_obj = Reference()
-        ref_obj.bibtex = stream.getvalue()
+	for key in bib_data.entries.keys():
+		stream = StringIO.StringIO()
+		writer.write_entry(key, bib_data.entries[key], stream)
+		title = getTitle(bib_data.entries[key].fields)
+		ref_obj = Reference()
+		ref_obj.bibtex = stream.getvalue()
 
-        try:
-            for field in bib_data.entries[key].fields:
-                value = bib_data.entries[key].fields[field]
+		try:
+			for field in bib_data.entries[key].fields:
+				value = bib_data.entries[key].fields[field]
 
-                if 'title' in field.lower():
-                    ref_obj.title = title
-                elif 'journal' in field.lower():
-                    ref_obj.journal = bibtex_purify(value)
-                elif 'year' in field.lower():
-                    ref_obj.year = int(value)
-                elif 'url' in field.lower():
-                    ref_obj.url = value
-                elif 'abstract' in field.lower():
-                    ref_obj.abstract = value
-                elif 'doi' in field.lower():
-                    doiPrepender = 'http://dx.doi.org/'
-                    if not value.startswith(doiPrepender):
-                        value = doiPrepender + value
-                    ref_obj.url = value
+				fieldname = field.strip().lower()
+				value = bibtex_purify(value)
 
-            authorsAsText = ""
-            count = 0
-            if bib_data.entries[key].persons:
-                numberOfAuthors = len(bib_data.entries[key].persons['author'])
-                for person in bib_data.entries[key].persons['author']:
+				if fieldname == 'year':
+					ref_obj.year = int(value)
+				elif fieldname == 'doi':
+					doiPrepender = 'http://dx.doi.org/'
+					if not value.startswith(doiPrepender):
+						value = doiPrepender + value
+					ref_obj.url = value
+				else:
+					setattr(ref_obj,fieldname,value)
 
-                    first = person.get_part_as_text('first')
-                    last = person.get_part_as_text('last')
+			authorsAsText = ""
+			count = 0
+			if bib_data.entries[key].persons:
+				numberOfAuthors = len(bib_data.entries[key].persons['author'])
+				for person in bib_data.entries[key].persons['author']:
 
-                    simpleAuthor = bibtex_purify(first + ' ' + last)
-                    if 'emph' in simpleAuthor:
-                        simpleAuthor = simpleAuthor.replace('emph', '')
+					first = person.get_part_as_text('first')
+					last = person.get_part_as_text('last')
 
-                    authorsAsText += simpleAuthor
+					simpleAuthor = bibtex_purify(first + ' ' + last)
+					if 'emph' in simpleAuthor:
+						simpleAuthor = simpleAuthor.replace('emph', '')
 
-                    if numberOfAuthors > 1:
-                        if count != (numberOfAuthors - 1):
-                            authorsAsText += ', '
-                        if count == (numberOfAuthors - 2):
-                            authorsAsText += ' and '
+					authorsAsText += simpleAuthor
 
-                    count += 1
+					if numberOfAuthors > 1:
+						if count != (numberOfAuthors - 1):
+							authorsAsText += ', '
+						if count == (numberOfAuthors - 2):
+							authorsAsText += ' and '
 
-            ref_obj.authorsAsText = authorsAsText
-            new_ref_id = family.add_reference(ref_obj)
-            recent_references.add_reference(ref_obj, new_ref_id, taxonomyItem.int_pk())
+					count += 1
 
-        except Exception, e:
-            print e
+			ref_obj.authorsAsText = authorsAsText
+			new_ref_id = family.add_reference(ref_obj)
+			recent_references.add_reference(ref_obj, new_ref_id, taxonomyItem.int_pk())
 
-    print "Imported %i BibTeX references." % len(bib_data.entries)
+		except Exception, e:
+			print "**** BIBTEX PARSING SCREWED UP ****"
+			print e
+			return
+
+	print "Imported %i BibTeX references." % len(bib_data.entries)
