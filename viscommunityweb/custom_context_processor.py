bf66078fdca1e726e31dab1e60d36320b41dd72e
Simon Walton <simon.walton@oerc.ox.ac.uk>
2013-10-10 12:41:44 +0100

    Corrected the custom preprocessor to use the settings.py's URL_PREPENDER rather than hard-coding /community

--- viscommunityweb/custom_context_processor.py
+++ viscommunityweb/custom_context_processor.py
@@ -4,4 +4,4 @@ from django.conf import settings
 
 def url_prepender(request):
     # return the value you want as a dictionnary. you may add multiple values in there.
-    return {'URL_PREPENDER': '/community'}
+    return {'URL_PREPENDER': settings.URL_PREPENDER}
